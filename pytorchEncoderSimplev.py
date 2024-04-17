10e042acfcb2acc0bfdf8941b5592fea2bd1442d
DavideB45 <dadewww@gmail.com>
2024-04-17 11:48:24 +0200

    now is smooth

--- pytorchEncoderSimplev.py
+++ pytorchEncoderSimplev.py
@@ -13,6 +13,10 @@ file_path = './Datasets/data.csv'
 data = pd.read_csv(file_path)
 data_array = data.to_numpy()
 data_array = data_array[:, 1:-1]
+#standardize each column independently
+data_array = (data_array - data_array.min(axis=0)) / (data_array.max(axis=0) - data_array.min(axis=0))
+#normalize each column independently
+#data_array = (data_array - data_array.mean(axis=0)) / data_array.std(axis=0)
 print(data_array.shape)
 #convert to float32
 data_array = data_array.astype('float32')
@@ -22,15 +26,15 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(in_features=data_array.shape[1], out_features=18, bias=True),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(in_features=18, out_features=15, bias=True),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(in_features=15, out_features=5, bias=True),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(in_features=5, out_features=15, bias=True),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(in_features=15, out_features=18, bias=True),
-            nn.ReLU(),
+            nn.Tanh(),
             nn.Linear(in_features=18, out_features=data_array.shape[1], bias=True)
         )
         #initialize the weights
@@ -55,14 +59,14 @@ model = NeuralNetwork().to(device)
 print(model)
 
 # Define the optimizer
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.95, nesterov=True)
 
 # Define the data loader
-batch_size = 64
+batch_size = 512
 data_loader = torch.utils.data.DataLoader(data_array, batch_size=batch_size, shuffle=True)
 
 # Train the network
-num_epochs = 20
+num_epochs = 90
 losses = []
 for epoch in range(num_epochs):
     for batch in data_loader:
