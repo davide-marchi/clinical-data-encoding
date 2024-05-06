4e79f1a3edf1a4c6724cb6d30c6157cbf3dd0382
DavideB45 <dadewww@gmail.com>
2024-05-06 16:16:57 +0200

    Update modelEncoderDecoderAdvancedV2.py
    can choose number of neurons but not the number of layers
    
    fit is now a function of IMEO class

--- modelEncoderDecoderAdvancedV2.py
+++ modelEncoderDecoderAdvancedV2.py
@@ -5,7 +5,7 @@ from torcheval.metrics.functional import r2_score
 torch.manual_seed(42)
 
 class IMEO(nn.Module):
-    def __init__(self, inputSize:int, total_binary_columns: int = 0, embedding_dim: int = 10):
+    def __init__(self, inputSize:int, total_binary_columns: int = 0, embedding_dim: int = 10, neurons_num:list = [40, 40, 30]):
         '''
         inputSize: int the number of data columns + mask columns (should be an even number)
         total_binary_columns: int the number of binary columns in the data (binary data should be the first columns)
@@ -15,30 +15,30 @@ class IMEO(nn.Module):
         self.total_binary_columns = total_binary_columns
         self.inputSize = inputSize
         self.encoder = nn.Sequential(
-            nn.Linear(in_features=inputSize, out_features=40, bias=True),
+            nn.Linear(in_features=inputSize, out_features=neurons_num[0], bias=True),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(40),
-            nn.Linear(in_features=40, out_features=40, bias=True),
+            nn.BatchNorm1d(neurons_num[0]),
+            nn.Linear(in_features=neurons_num[0], out_features=neurons_num[1], bias=True),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(40),
-            nn.Linear(in_features=40, out_features=30, bias=True),
+            nn.BatchNorm1d(neurons_num[1]),
+            nn.Linear(in_features=neurons_num[1], out_features=neurons_num[2], bias=True),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(30),
-            nn.Linear(in_features=30, out_features=embedding_dim, bias=True),
+            nn.BatchNorm1d(neurons_num[2]),
+            nn.Linear(in_features=neurons_num[2], out_features=embedding_dim, bias=True),
             nn.LeakyReLU(),
         )
         self.decoder = nn.Sequential(
             nn.BatchNorm1d(embedding_dim),
-            nn.Linear(in_features=embedding_dim, out_features=30, bias=True),
+            nn.Linear(in_features=embedding_dim, out_features=neurons_num[2], bias=True),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(30), 
-            nn.Linear(in_features=30, out_features=40, bias=True),
+            nn.BatchNorm1d(neurons_num[2]), 
+            nn.Linear(in_features=neurons_num[2], out_features=neurons_num[1], bias=True),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(40),
-            nn.Linear(in_features=40, out_features=40, bias=True),
+            nn.BatchNorm1d(neurons_num[1]),
+            nn.Linear(in_features=neurons_num[1], out_features=neurons_num[0], bias=True),
             nn.LeakyReLU(),
-            nn.BatchNorm1d(40),
-            nn.Linear(in_features=40, out_features=40, bias=True),
+            nn.BatchNorm1d(neurons_num[0]),
+            nn.Linear(in_features=neurons_num[0], out_features=neurons_num[0], bias=True),
             nn.LeakyReLU()
         )
         self.binaOut = nn.Linear(in_features=40, out_features=total_binary_columns, bias=True)
@@ -161,58 +161,65 @@ class IMEO(nn.Module):
             metrics_dict['a2'] = self.weighted_measure(batch, binCol)
         return metrics_dict
     
-def trainIMEO(  
-                model:IMEO, 
-                train_data:torch.Tensor, 
-                val_data:torch.Tensor, 
-                optimizer:torch.optim.Optimizer, 
-                binary_clumns:int, 
-                device:torch.device,
-                num_epochs:int = 100, 
-                batch_size:int = 64, 
-                binary_loss_weight:float = 0.5,
-                print_every:int = 200,
-                plot:bool = True,
-                metrics:list = ['tr_loss', 'val_loss', 'val_r2', 'val_acc', 'val_a2'],
-            ) -> dict:
-    
-    data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    metrics_hystory = {metric:[] for metric in metrics}
-    for i in range(num_epochs):
-        for batch in data_loader:
-            optimizer.zero_grad()
-            batch = batch.to(device)
-            loss = model.training_step(batch, binaryLossWeight=binary_loss_weight)
-            loss.backward()
-            optimizer.step()
-        for metric in metrics:
-            if metric == 'tr_loss':
-                metrics_hystory[metric].append(model.training_step(train_data).item())
-            elif metric == 'val_loss':
-                metrics_hystory[metric].append(model.training_step(val_data).item())
-            elif metric == 'val_r2':
-                metrics_hystory[metric].append(model.compute_r_squared(val_data, binary_clumns))
-            elif metric == 'val_acc':
-                metrics_hystory[metric].append(model.compute_accuracy(val_data, binary_clumns))
-            elif metric == 'val_a2':
-                metrics_hystory[metric].append(model.weighted_measure(val_data, binary_clumns))
-        if (print_every != 0):
-            print(f"Epoch [{i+1}/{num_epochs}]",
-            f"Loss: {metrics_hystory['tr_loss'][-1]:.4f}" if 'tr_loss' in metrics else '',
-            f"Val Loss: {metrics_hystory['val_loss'][-1]:.4f}" if 'val_loss' in metrics else '',
-            f"Val R^2: {metrics_hystory['val_r2'][-1]:.2f}" if 'val_r2' in metrics else '',
-            f"Val acc: {metrics_hystory['val_acc'][-1]:.2f}" if 'val_acc' in metrics else '',
-            f"New a^2: {metrics_hystory['val_a2'][-1]:.2f}" if 'val_a2' in metrics else '',
-            "      ", end='\r')
-        if print_every != 0 and i % print_every == 0:
-            print('')
-    print('\nTraining complete\t\t\t\t')
-    if plot:
-        import matplotlib.pyplot as plt
-        fig, ax = plt.subplots(1, len(metrics), figsize=(20, 5))
-        for i, metric in enumerate(metrics):
-            ax[i].plot(metrics_hystory[metric])
-            ax[i].set_title(metric)
-        plt.show()
-    return metrics_hystory
-    
+    def freeze(self):
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self):
+        for param in self.parameters():
+            param.requires_grad = True
+
+    def fit(self,
+            train_data:torch.Tensor, 
+            val_data:torch.Tensor, 
+            optimizer:torch.optim.Optimizer, 
+            device:torch.device,
+            num_epochs:int = 100, 
+            batch_size:int = 64, 
+            binary_loss_weight:float = None,
+            print_every:int = 200,
+            plot:bool = True,
+            metrics:list = ['tr_loss', 'val_loss', 'val_r2', 'val_acc', 'val_a2'],
+        ) -> dict:
+        binary_loss_weight = self.total_binary_columns / (self.inputSize // 2) if binary_loss_weight is None else binary_loss_weight
+        data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+        metrics_hystory = {metric:[] for metric in metrics}
+        for i in range(num_epochs):
+            for batch in data_loader:
+                optimizer.zero_grad()
+                batch = batch.to(device)
+                loss = self.training_step(batch, binaryLossWeight=binary_loss_weight)
+                loss.backward()
+                optimizer.step()
+            for metric in metrics:
+                if metric == 'tr_loss':
+                    metrics_hystory[metric].append(self.training_step(train_data).item())
+                elif metric == 'val_loss':
+                    metrics_hystory[metric].append(self.training_step(val_data).item())
+                elif metric == 'val_r2':
+                    metrics_hystory[metric].append(self.compute_r_squared(val_data, self.total_binary_columns))
+                elif metric == 'val_acc':
+                    metrics_hystory[metric].append(self.compute_accuracy(val_data, self.total_binary_columns))
+                elif metric == 'val_a2':
+                    metrics_hystory[metric].append(self.weighted_measure(val_data, self.total_binary_columns))
+            if (print_every != 0):
+                with torch.no_grad():
+                    print(f"Epoch [{i+1}/{num_epochs}]",
+                    f"Loss: {metrics_hystory['tr_loss'][-1]:.4f}" if 'tr_loss' in metrics else '',
+                    f"Val Loss: {metrics_hystory['val_loss'][-1]:.4f}" if 'val_loss' in metrics else '',
+                    f"Val R^2: {metrics_hystory['val_r2'][-1]:.2f}" if 'val_r2' in metrics else '',
+                    f"Val acc: {metrics_hystory['val_acc'][-1]:.2f}" if 'val_acc' in metrics else '',
+                    f"New a^2: {metrics_hystory['val_a2'][-1]:.2f}" if 'val_a2' in metrics else '',
+                    "      ", end='\r')
+            if print_every != 0 and i % print_every == 0:
+                print('')
+        print('\nTraining complete\t\t\t\t')
+        if plot:
+            import matplotlib.pyplot as plt
+            fig, ax = plt.subplots(1, len(metrics), figsize=(20, 5))
+            for i, metric in enumerate(metrics):
+                ax[i].plot(metrics_hystory[metric])
+                ax[i].set_title(metric)
+            plt.show()
+        return metrics_hystory
+        
