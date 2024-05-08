cd9635eea8bc2a08ba68a51dbd2d0cbcbd6bc41e
DavideB45 <dadewww@gmail.com>
2024-05-08 09:57:54 +0200

    moved files

---
+++ obsolete/classifierModelGenralFirstVersionBasic.py
@@ -0,0 +1,147 @@
+from matplotlib import pyplot as plt
+import torch
+import torch.nn as nn
+import torch.utils
+from torcheval.metrics.functional import binary_accuracy
+from torch.utils.data import DataLoader, Dataset
+
+torch.manual_seed(42)
+
+class ClassifierBinary(nn.Module):
+    def __init__(self, inputSize:int) -> None:
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(in_features=inputSize, out_features=40, bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(40),
+            nn.Linear(in_features=40, out_features=40, bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(40),
+            nn.Linear(in_features=40, out_features=30, bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(30),
+            nn.Linear(in_features=30, out_features=1, bias=True),
+            nn.Sigmoid()
+        )
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight)
+                nn.init.uniform_(m.bias, -0.5, 0.5)
+    
+    def saveModel(self, path):
+        torch.save(self.state_dict(), path)
+
+    def forward(self, x):
+        return self.network(x)
+    
+    def binary_loss(self, output:torch.Tensor, target:torch.Tensor) -> torch.Tensor:
+        output = output.squeeze()
+        target = target.squeeze()
+        return nn.functional.binary_cross_entropy(output, target)
+        print(f'x: {x.shape}')
+        print(f'y: {y.shape}')
+        y_hat = self(x)
+        return nn.functional.binary_cross_entropy(y_hat, y)
+    
+    def compute_accuracy(self, input:torch.Tensor, target:torch.Tensor) -> float:
+        input = input.squeeze()
+        target = target.squeeze()
+        return binary_accuracy(input, target)
+    
+    def reset(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight)
+                nn.init.uniform_(m.bias, -0.5, 0.5)
+
+    def fit(self, train_data, tr_out, val_data, val_out, optimizer, device, num_epochs, batch_size, print_every=10, plot=True, preprocess= lambda x:x):
+
+        train_losses = []
+        val_losses = []
+        train_accuracies = []
+        val_accuracies = []
+        train_loader = DataLoader(DatasetClassificator(train_data, tr_out), batch_size=batch_size, shuffle=True)
+        val_loader = DataLoader(DatasetClassificator(val_data, val_out), batch_size=batch_size)
+
+        self.to(device)
+        self.train()
+
+        for epoch in range(num_epochs):
+            epoch_train_loss = 0.0
+            epoch_val_loss = 0.0
+            epoch_train_accuracy = 0.0
+            epoch_val_accuracy = 0.0
+
+            for batch in train_loader:
+                optimizer.zero_grad()
+
+                x, y = batch
+                x = preprocess(x)
+                x = x.to(device)
+                y = y.to(device)
+
+                y_hat = self(x)
+                loss = self.binary_loss(y_hat, y)
+                loss.backward()
+                optimizer.step()
+
+                epoch_train_loss += loss.item()
+                epoch_train_accuracy += self.compute_accuracy(y_hat, y)
+
+            with torch.no_grad():
+                for batch in val_loader:
+                    x, y = batch
+                    x = preprocess(x)
+                    x = x.to(device)
+                    y = y.to(device)
+
+                    y_hat = self(x)
+                    loss = self.binary_loss(y_hat, y)
+
+                    epoch_val_loss += loss.item()
+                    epoch_val_accuracy += self.compute_accuracy(y_hat, y)
+
+            epoch_train_loss /= len(train_loader)
+            epoch_val_loss /= len(val_loader)
+            epoch_train_accuracy /= len(train_loader)
+            epoch_val_accuracy /= len(val_loader)
+
+            train_losses.append(epoch_train_loss)
+            val_losses.append(epoch_val_loss)
+            train_accuracies.append(epoch_train_accuracy)
+            val_accuracies.append(epoch_val_accuracy)
+
+            if (epoch + 1) % print_every == 0:
+                print(f"Epoch {epoch+1}/{num_epochs}: Train Loss: {epoch_train_loss:.4f}, Val Loss: {epoch_val_loss:.4f}, Train Accuracy: {epoch_train_accuracy:.4f}, Val Accuracy: {epoch_val_accuracy:.4f}")
+
+        if plot:
+            plt.figure(figsize=(12, 4))
+            plt.subplot(1, 2, 1)
+            plt.plot(train_losses, label='Train Loss')
+            plt.plot(val_losses, label='Val Loss')
+            plt.xlabel('Epoch')
+            plt.ylabel('Loss')
+            plt.legend()
+
+            plt.subplot(1, 2, 2)
+            plt.plot(train_accuracies, label='Train Accuracy')
+            plt.plot(val_accuracies, label='Val Accuracy')
+            plt.xlabel('Epoch')
+            plt.ylabel('Accuracy')
+            plt.legend()
+
+            plt.tight_layout()
+            plt.show()
+
+        return train_losses, val_losses, train_accuracies, val_accuracies
+    
+class DatasetClassificator(Dataset):
+    def __init__(self, data, target):
+        self.data = data
+        self.target = target
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx], self.target[idx]
---
+++ obsolete/modelEncoderDecoderAdvancedV2.py
@@ -0,0 +1,231 @@
+import torch
+import torch.nn as nn
+from torcheval.metrics.functional import r2_score
+
+torch.manual_seed(42)
+
+class IMEO(nn.Module):
+    def __init__(self, inputSize:int, total_binary_columns: int = 0, embedding_dim: int = 10, neurons_num:list = [40, 40, 30]):
+        '''
+        inputSize: int the number of data columns + mask columns (should be an even number)
+        total_binary_columns: int the number of binary columns in the data (binary data should be the first columns)
+        embedding_dim: int the number of neurons in the embedding layer
+        '''
+        super().__init__()
+        self.total_binary_columns = total_binary_columns
+        self.inputSize = inputSize
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=inputSize, out_features=neurons_num[0], bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(neurons_num[0]),
+            nn.Linear(in_features=neurons_num[0], out_features=neurons_num[1], bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(neurons_num[1]),
+            nn.Linear(in_features=neurons_num[1], out_features=neurons_num[2], bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(neurons_num[2]),
+            nn.Linear(in_features=neurons_num[2], out_features=embedding_dim, bias=True),
+            nn.LeakyReLU(),
+        )
+        self.decoder = nn.Sequential(
+            nn.BatchNorm1d(embedding_dim),
+            nn.Linear(in_features=embedding_dim, out_features=neurons_num[2], bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(neurons_num[2]), 
+            nn.Linear(in_features=neurons_num[2], out_features=neurons_num[1], bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(neurons_num[1]),
+            nn.Linear(in_features=neurons_num[1], out_features=neurons_num[0], bias=True),
+            nn.LeakyReLU(),
+            nn.BatchNorm1d(neurons_num[0]),
+            nn.Linear(in_features=neurons_num[0], out_features=neurons_num[0], bias=True),
+            nn.LeakyReLU()
+        )
+        self.binaOut = nn.Linear(in_features=40, out_features=total_binary_columns, bias=True)
+        self.binActivation = nn.Sigmoid()
+        self.contOut = nn.Linear(in_features=40, out_features=inputSize//2 - total_binary_columns, bias=True)
+        #initialize the weights
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight)
+                nn.init.uniform_(m.bias, -0.5, 0.5)
+    
+    def saveModel(self, path):
+        torch.save(self.state_dict(), path)
+
+    def loadModel(self, path):
+        self.load_state_dict(torch.load(path))
+
+    def reset(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_uniform_(m.weight)
+                nn.init.uniform_(m.bias, -0.5, 0.5)
+
+    def forward(self, x):
+        '''
+        x: tensor of shape (batch_size, inputSize)
+        return: tensor of shape (batch_size, inputSize//2)
+        '''
+        x = self.encoder(x)
+        x = self.decoder(x)
+        x1 = self.binActivation(self.binaOut(x))
+        x2 = self.contOut(x)
+        x = torch.cat((x1, x2), dim=1)
+        return x
+    
+    def encode(self, x):
+        '''
+        x: tensor of shape (batch_size, inputSize)
+        return: tensor of shape (batch_size, embedding_dim)
+        '''
+        return self.encoder(x)
+    
+    def compute_r_squared(self, batch, binCol: int = 0):
+        '''
+        batch: tensor of shape (batch_size, inputSize)
+        binCol: int the number of binary columns in the data (binary data should be the first columns)
+            if omitted, the total_binary_columns used during creation of the NN will be used
+        return: float the r squared value
+        '''
+        x = batch
+        val, mask = torch.chunk(x, 2, dim=1)
+        y_hat = torch.mul(self(x), mask)
+        y = torch.mul(val, mask)
+        if self.total_binary_columns != 0 or binCol != 0:
+            binCol = self.total_binary_columns if binCol == 0 else binCol
+            y_hat = y_hat[:, :-binCol]
+            y = y[:, :-binCol]
+        return r2_score(y, y_hat).item()
+    
+    def compute_accuracy(self, batch, binCol: int = 0):
+        '''
+        batch: tensor of shape (batch_size, inputSize)
+        binCol: int the number of binary columns in the data (binary data should be the first columns)
+            if omitted, the total_binary_columns used during creation of the NN will be used
+        return: float the accuracy value
+        '''
+        if self.total_binary_columns == 0 and binCol == 0:
+            return 0
+        x = batch
+        binCol = self.total_binary_columns if binCol == 0 else binCol
+        val, mask = torch.chunk(x, 2, dim=1)
+        y_hat = torch.mul(self(x), mask)
+        y = torch.mul(val, mask)
+        y_hat_bin = y_hat[:, :binCol]
+        y_bin = y[:, :binCol]
+        y_hat_bin = torch.round(y_hat_bin)
+        return (torch.sum(y_hat_bin == y_bin) / (y_hat_bin.shape[0] * binCol)).item()
+    
+    def weighted_measure(self, batch, binCol: int = 0):
+        acc = self.compute_accuracy(batch, binCol)
+        r2 = self.compute_r_squared(batch, binCol)
+        if self.total_binary_columns == 0 and binCol == 0:
+            return 0
+        binCol = self.total_binary_columns if binCol == 0 else binCol
+        binWeight = binCol / (self.inputSize // 2)
+        return acc * binWeight + r2 * (1 - binWeight)
+    
+    def training_step(self, batch, binaryLossWeight: float = 0.5, forceMSE: bool = False):
+        '''
+        batch: tensor of shape (batch_size, inputSize)
+        binaryLossWeight: float the weight of the binary loss in the total loss (0 <= binaryLossWeight <= 1)
+            the weight of the continuous loss will be 1 - binaryLossWeight
+        return: the total loss
+        '''
+        x = batch
+        val, mask = torch.chunk(x, 2, dim=1)
+        y_hat = torch.mul(self(x), mask)
+        y = torch.mul(val, mask)
+        if self.total_binary_columns == 0 or forceMSE:
+            return nn.functional.mse_loss(y, y_hat)
+        y_hat_bin = y_hat[:, :self.total_binary_columns]
+        y_bin = y[:, :self.total_binary_columns]
+        loss_bin = nn.functional.binary_cross_entropy(y_hat_bin, y_bin)
+        y_hat_cont = y_hat[:, :-self.total_binary_columns]
+        y_cont = y[:, :-self.total_binary_columns]
+        loss_cont = nn.functional.mse_loss(y_cont, y_hat_cont)
+        loss = binaryLossWeight*loss_bin + (1 - binaryLossWeight)*loss_cont
+        return loss
+    
+    def compute_metrics(self, batch, binCol: int = 0, metrics:list = ['loss', 'r2', 'acc', 'a2']):
+        '''
+        batch: tensor of shape (batch_size, inputSize)
+        binCol: int the number of binary columns in the data (binary data should be the first columns)
+            if omitted, the total_binary_columns used during creation of the NN will be used
+        metrics: list the metrics to compute
+        return: dict the computed metrics
+        '''
+        metrics_dict = {}
+        if 'loss' in metrics:
+            metrics_dict['loss'] = self.training_step(batch).item()
+        if 'r2' in metrics:
+            metrics_dict['r2'] = self.compute_r_squared(batch, binCol)
+        if 'acc' in metrics:
+            metrics_dict['acc'] = self.compute_accuracy(batch, binCol)
+        if 'a2' in metrics:
+            metrics_dict['a2'] = self.weighted_measure(batch, binCol)
+        return metrics_dict
+    
+    def freeze(self):
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def unfreeze(self):
+        for param in self.parameters():
+            param.requires_grad = True
+
+    def fit(self,
+            train_data:torch.Tensor, 
+            val_data:torch.Tensor, 
+            optimizer:torch.optim.Optimizer, 
+            device:torch.device,
+            num_epochs:int = 100, 
+            batch_size:int = 64, 
+            binary_loss_weight:float = None,
+            print_every:int = 200,
+            plot:bool = True,
+            metrics:list = ['tr_loss', 'val_loss', 'val_r2', 'val_acc', 'val_a2'],
+        ) -> dict:
+        binary_loss_weight = self.total_binary_columns / (self.inputSize // 2) if binary_loss_weight is None else binary_loss_weight
+        data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+        metrics_hystory = {metric:[] for metric in metrics}
+        for i in range(num_epochs):
+            for batch in data_loader:
+                optimizer.zero_grad()
+                batch = batch.to(device)
+                loss = self.training_step(batch, binaryLossWeight=binary_loss_weight)
+                loss.backward()
+                optimizer.step()
+            for metric in metrics:
+                if metric == 'tr_loss':
+                    metrics_hystory[metric].append(self.training_step(train_data).item())
+                elif metric == 'val_loss':
+                    metrics_hystory[metric].append(self.training_step(val_data).item())
+                elif metric == 'val_r2':
+                    metrics_hystory[metric].append(self.compute_r_squared(val_data, self.total_binary_columns))
+                elif metric == 'val_acc':
+                    metrics_hystory[metric].append(self.compute_accuracy(val_data, self.total_binary_columns))
+                elif metric == 'val_a2':
+                    metrics_hystory[metric].append(self.weighted_measure(val_data, self.total_binary_columns))
+            if (print_every != 0):
+                with torch.no_grad():
+                    print(f"Epoch [{i+1}/{num_epochs}]",
+                    f"Loss: {metrics_hystory['tr_loss'][-1]:.4f}" if 'tr_loss' in metrics else '',
+                    f"Val Loss: {metrics_hystory['val_loss'][-1]:.4f}" if 'val_loss' in metrics else '',
+                    f"Val R^2: {metrics_hystory['val_r2'][-1]:.2f}" if 'val_r2' in metrics else '',
+                    f"Val acc: {metrics_hystory['val_acc'][-1]:.2f}" if 'val_acc' in metrics else '',
+                    f"New a^2: {metrics_hystory['val_a2'][-1]:.2f}" if 'val_a2' in metrics else '',
+                    "      ", end='\r')
+            if print_every != 0 and i % print_every == 0:
+                print('')
+        print('\nTraining complete\t\t\t\t')
+        if plot:
+            import matplotlib.pyplot as plt
+            fig, ax = plt.subplots(1, len(metrics), figsize=(20, 5))
+            for i, metric in enumerate(metrics):
+                ax[i].plot(metrics_hystory[metric])
+                ax[i].set_title(metric)
+            plt.show()
+        return metrics_hystory
+        
