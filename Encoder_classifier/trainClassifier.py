3a8fa60ca66c9a8821eff2da6912199473b4981f
DavideB45 <dadewww@gmail.com>
2024-05-08 12:11:29 +0200

    train encoder decoder

--- Encoder_classifier/trainClassifier.py
+++ Encoder_classifier/trainClassifier.py
@@ -1,9 +1,11 @@
 from modelEncoderDecoderAdvancedV2 import IMEO
+from classifier import ClassifierBinary
 import torch
 import os
 import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from utilsData import dataset_loader, load_data
+
 
 device = torch.device(  "cuda" if torch.cuda.is_available() 
                         else  "mps" if torch.backends.mps.is_available()
@@ -24,56 +26,44 @@ val_data = dict['val_data']
 val_out = dict['val_out']
 binary_clumns = dict['bin_col']
 
-
-binary_loss_weight = binary_clumns / (tr_data.shape[1]//2)
 batch_size = 100
 learning_rate = 0.002
 plot = False
-embedding_dim = 17
 weight_decay = 0.5e-5
 num_epochs = 300
 
 print(f'Number of binary columns: {binary_clumns}')
 print(f'Total number of columns: {tr_data.shape[1]/2}')
-print(f'Binary loss weight: {binary_loss_weight:.3}')
-print(f'Batch size: {batch_size}')
 print(f'Learning rate: {learning_rate}')
-print(f'Embedding dimension: {embedding_dim}')
 print(f'Weight decay: {weight_decay}')
 print(f'Number of epochs: {num_epochs}')
 
-encoder_decoder = IMEO(inputSize=tr_data.shape[1], total_binary_columns=binary_clumns, embedding_dim=embedding_dim)
+#encoder_decoder:IMEO = torch.load('./Encoder_classifier/encoder_decoder.pth')
 
-print(encoder_decoder)
-
-encoder_decoder.to(device)
-
-
-optimizer = torch.optim.Adam(encoder_decoder.parameters(), 
-                             weight_decay=weight_decay, 
-                             lr=learning_rate)
+#encoder_decoder.to(device)
 
 # Train the network
-losses_tr = []
-losses_val = []
-accuracy = [0]
-r_squared = []
-weighed_goodness = []
 train_data = tr_data.to(device)
 val_data = val_data.to(device)
 data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
 
-history = encoder_decoder.fit(train_data,
-                    val_data,
-                    optimizer,
-                    device=device,
-                    num_epochs=num_epochs,
-                    batch_size=batch_size,
-                    binary_loss_weight=binary_loss_weight,
-                    print_every=num_epochs//5,
-                    plot=plot
-                )
+#embedding_dim = encoder_decoder.encoder[-2].out_features
+
+classifier = ClassifierBinary(inputSize=train_data.shape[1])
+
+classifier.to(device)
+history = classifier.fit(train_data, 
+                         tr_out, 
+                         val_data, 
+                         val_out, 
+                         optimizer=torch.optim.Adam(classifier.parameters(), lr=learning_rate), 
+                         device=device, 
+                         num_epochs=num_epochs, 
+                         batch_size=batch_size, 
+                         #preprocess=encoder_decoder.encode
+                         )
+
 print('\n\nModel Trained\n\n')
 print('Saving model...')
-encoder_decoder.saveModel('./Encoder_classifier/encoder_decoder.pth')
+#encoder_decoder.saveModel('./Classifier/classifier.pth')
 print('Model saved')
