5ea9283db39dee80819279ab50a4605a98baef3f
giordanoscerra <121350206+giordanoscerra@users.noreply.github.com>
2024-05-21 16:32:13 +0200

    started dynamic distribution of hidden size

--- Encoder_classifier/trainEncoder.py
+++ Encoder_classifier/trainEncoder.py
@@ -46,9 +46,9 @@ print(f'Number of epochs: {num_epochs}')
 encoder_decoder = IMEO(
     inputSize=tr_data.shape[1], 
     total_binary_columns=binary_clumns, 
-    embedding_dim=embedding_dim,
-    neurons_num=[100, 80, 40]
+    embedding_percentage=25.0
     )
+
 
 #print(encoder_decoder)
 
