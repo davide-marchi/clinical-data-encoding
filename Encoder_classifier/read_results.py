02fd230caa94423913bdcaeeb380d210cb421819
Davide <davidemarchi00@gmail.com>
2024-05-27 15:41:56 +0200

    Maybe fixed plots (maybe not)

--- Encoder_classifier/read_results.py
+++ Encoder_classifier/read_results.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from scipy.interpolate import griddata
 from numpy import unique
+from itertools import product
 
 
 
@@ -11,19 +12,23 @@ from numpy import unique
 with open('Encoder_classifier/Models/results.json', 'r') as file:
     results = json.load(file)
 
-X_list = unique(sorted([model['embedding_perc'] for model in results]))
-Y_list = unique(sorted([model['masked_percentage'] for model in results]))
+X_unique = unique(sorted([model['embedding_perc'] for model in results]))
+Y_unique = unique(sorted([model['masked_percentage'] for model in results]))
 
+X_list = []
+Y_list = []
 Z_list = []
-for embedding_perc, masked_percentage in zip(X_list, Y_list):
+
+for embedding_perc, masked_percentage in product(X_unique, Y_unique):
     # check if this is working correctly
     max_f1_score = max([model['report']['macro avg']['f1-score'] for model in results if model['embedding_perc'] == embedding_perc and model['masked_percentage'] == masked_percentage])
     Z_list.append(max_f1_score)
+    X_list.append(embedding_perc)
+    Y_list.append(masked_percentage)
     
-
 #Z_list = [model['report']['macro avg']['f1-score'] for model in results]
 
-X, Y = np.meshgrid(unique(sorted(X_list)), unique(sorted(Y_list)))
+X, Y = np.meshgrid(X_unique, Y_unique)
 
 # Interpola i valori Z per la griglia
 Z = griddata((X_list, Y_list), Z_list, (X, Y), method='nearest') # TODO: Controllare se funzioni
@@ -46,7 +51,7 @@ ax.contourf(X, Y, Z, zdir='z', offset=-0.2, cmap='coolwarm')
 ax.contourf(X, Y, Z, zdir='x', offset=-0.2, cmap='coolwarm')
 ax.contourf(X, Y, Z, zdir='y', offset=-0.2, cmap='coolwarm')
 
-ax.set(xlim=(-0.2, 1.2), ylim=(-0.2, 1.2), zlim=(-0.2, 1.2), xlabel='Embedding percentage', ylabel='Masked percentage', zlabel='Macro avg F1-score')
+ax.set(xlim=(-0.2, 1.2), ylim=(-0.2, 1.2), zlim=(0.6, 0.8), xlabel='Embedding percentage', ylabel='Masked percentage', zlabel='Macro avg F1-score')
 
 plt.savefig('Encoder_classifier/Models/3dPlot.png')
 plt.show()
