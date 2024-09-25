63f84d7c3ef28c0a5be8a9293fe78c4894c8d639
DavideB45 <dadewww@gmail.com>
2024-09-26 00:47:32 +0200

    Update main_3.py
    fixed warnings
    gridsearch parames
    early stoppings

--- Encoder_classifier/main_3.py
+++ Encoder_classifier/main_3.py
@@ -1,9 +1,9 @@
 #only because i don't wnat ot break the other main.py
 import warnings
-#warnings.simplefilter(action='ignore', category=FutureWarning, append=True)
+#warnings.simplefilter(action='ignore', category=FutureWarning)
 
 from time import time
-from classifier import ClassifierBinary, BCEWeightedLoss
+from classifier import ClassifierBinary
 from modelEncoderDecoderAdvancedV2 import IMEO
 from itertools import product
 import torch
@@ -16,23 +16,24 @@ from sklearn.metrics import classification_report, f1_score
 from skorch import NeuralNetClassifier
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingGridSearchCV
+from skorch.callbacks import EarlyStopping
 
 # YEARS TO PREDICT
 years_to_death = 8
 # CLASSIFIER PARAMETERS
 param_grid = {
-        'optimizer__lr' : [0.001, 0.3],
-        'optimizer__weight_decay' : [0.1e-5, 0.5e-5],
+        'optimizer__lr' : [0.0005, 0.001, 0.005, 0.01],
+        'optimizer__weight_decay' : [0.01e-5, 0.05e-5, 0.1e-5, 0.5e-5],
         'max_epochs' : [50],
-        'batch_size' : [100, 200]
+        'batch_size' : [100, 300]
     }
 # ENCODER PARAMETERS
-EN_binary_loss_weight = [ 0.001, 0.01, 0.0002, 0.0005, None]# very important
-EN_batch_size = [200]
-EN_learning_rate = [0.0015]
+EN_binary_loss_weight = [ 0.001, 0.01, 0.0001, None]# very important
+EN_batch_size = [300]
+EN_learning_rate = [0.0015, 0.003]
 EN_plot = False
-EN_embedding_perc_list = [2, 1.5, 3, 1, 0.8]
-EN_weight_decay = [0.05e-5, 0.2e-5]
+EN_embedding_perc_list = [2, 3, 0.8, 0.5]
+EN_weight_decay = [0.05e-5, 0.3e-5]
 EN_num_epochs = [250]
 EN_masked_percentage_list = [ 0.3, 0.4, 0.5, 0.6]
 EN_patience = [10]
@@ -60,79 +61,86 @@ print(f'Shape of tr_out: {tr_out.shape}')
 results,    existing_models,        validated_models = load_past_results_and_models()
 
 begin = time()
-for en_bin_loss_w, en_bs, en_lr, en_emb_perc, en_wd, en_num_ep, en_masked_perc, en_pt \
-        in product(EN_binary_loss_weight, EN_batch_size, EN_learning_rate, EN_embedding_perc_list, EN_weight_decay, EN_num_epochs, EN_masked_percentage_list, EN_patience):
-    
+combinations = list(product(EN_binary_loss_weight, EN_batch_size, EN_learning_rate, EN_embedding_perc_list, EN_weight_decay, EN_num_epochs, EN_masked_percentage_list, EN_patience))
+combinations.insert(0, (None,)*len(combinations[0]))
+print(f'Number of combinations: {len(combinations)}')
+for comb in combinations:
+    en_bin_loss_w, en_bs, en_lr, en_emb_perc, en_wd, en_num_ep, en_masked_perc, en_pt = comb
+    torch.manual_seed(42)
+
     encoder_string = f'encoder_{en_bin_loss_w}_{en_bs}_{en_lr}_{en_emb_perc}_{en_wd}_{en_num_ep}_{en_masked_perc}_{en_pt}'
 
     ################################################################################################
     # TRAIN ENCODER ################################################################################
     ################################################################################################
-    if encoder_string in validated_models:
-        print(f'{encoder_string} already validated')
-        continue
-    # check if encoder exists
-    if encoder_string + '.pth' in existing_models:
-        # load encoder
-        encoder:IMEO = torch.load('./Encoder_classifier/gridResults/Models/' + encoder_string + '.pth')
-    else:
-        # create, train and save encoder
-        encoder = IMEO(
-            inputSize=tr_data.shape[1], 
-            total_binary_columns=binary_clumns, 
-            embedding_percentage=en_emb_perc,
+    if comb != (None,)*len(comb):
+        if encoder_string in validated_models:
+            print(f'{encoder_string} already validated')
+            continue
+        # check if encoder exists
+        if encoder_string + '.pth' in existing_models:
+            # load encoder
+            encoder:IMEO = torch.load('./Encoder_classifier/gridResults/Models/' + encoder_string + '.pth', weights_only=False)
+        else:
+            # create, train and save encoder
+            encoder = IMEO(
+                inputSize=tr_data.shape[1], 
+                total_binary_columns=binary_clumns, 
+                embedding_percentage=en_emb_perc,
+                )
+            encoder.fit(
+                extended_tr_data, 
+                val_data,
+                optimizer = torch.optim.Adam(encoder.parameters(), 
+                                            weight_decay=en_wd, 
+                                            lr=en_lr
+                                            ),
+                device = device,
+                batch_size = en_bs,
+                plot = EN_plot,
+                num_epochs = en_num_ep,
+                masked_percentage = en_masked_perc,
+                early_stopping=en_pt,
+                binary_loss_weight=en_bin_loss_w,
+                print_every=en_num_ep,
             )
-        encoder.fit(
-            extended_tr_data, 
-            val_data,
-            optimizer = torch.optim.Adam(encoder.parameters(), 
-                                        weight_decay=en_wd, 
-                                        lr=en_lr
-                                        ),
-            device = device,
-            batch_size = en_bs,
-            plot = EN_plot,
-            num_epochs = en_num_ep,
-            masked_percentage = en_masked_perc,
-            early_stopping=en_pt,
-            binary_loss_weight=en_bin_loss_w,
-            print_every=en_num_ep,
-        )
-        encoder.saveModel(f'./Encoder_classifier/gridResults/Models/{encoder_string}.pth')
-        existing_models.append(encoder_string + '.pth')
+            encoder.saveModel(f'./Encoder_classifier/gridResults/Models/{encoder_string}.pth')
+            existing_models.append(encoder_string + '.pth')
 
-    encoder.freeze()
+        encoder.freeze()
+        encoded_tr_data = encoder.encode(tr_data)
+        val_data_encoded = encoder.encode(val_data)
+        embedding_dim = encoder.embedding_dim
+    else:
+        encoded_tr_data = tr_data.clone().detach()
+        val_data_encoded = val_data.clone().detach()
+        embedding_dim = tr_data.shape[1]
 
     ################################################################################################
     # TRAIN CLASSIFIER #############################################################################
     ################################################################################################
 
-    encoded_tr_data = encoder.encode(tr_data)
 
     model = NeuralNetClassifier(
         module = ClassifierBinary,
-        module__inputSize = encoder.embedding_dim,
+        module__inputSize = embedding_dim,
         optimizer = torch.optim.Adam,
         device = device,
-        criterion=torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([posWeight])),
+        criterion=torch.nn.BCEWithLogitsLoss(pos_weight=torch.tensor([posWeight])),#TODO: this will be a hyperparameter (if they pay us enough)
         verbose=0,
-        callbacks='disable'
+        callbacks=[('early_stopping', EarlyStopping(patience=10))]
     )
 
     grid = HalvingGridSearchCV(estimator=model, 
                                param_grid=param_grid, 
                                n_jobs=-1, 
-                               verbose=1,
-                               cv=3,
+                               verbose=0,
                                scoring='f1_macro',
+                               random_state=42,
                                )
-    # Reshape tr_out to be 2D
-    formatted_tr_out = tr_out.reshape(-1, 1)
-
-    # Use formatted_tr_out in the fit method
-    grid_result = grid.fit(encoded_tr_data, formatted_tr_out)
     
-    y_pred = grid.predict(encoder.encode(val_data))
+    grid_result = grid.fit(encoded_tr_data, tr_out)
+    y_pred = grid.predict(val_data_encoded)
     report = classification_report(val_out, y_pred, output_dict=True)
 
     results.append({
